39e20c11ca4de066c8716c05b5f23fe6d758a538
walid <waliddari69gmail.com>
2026-01-24 14:51:05 +0100

    frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/",
   resolve: {
     alias: {
       "@mui/styled-engine": "@mui/styled-engine","@": path.resolve(__dirname, "./src"),
